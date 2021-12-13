0aefcab4103297c63e9ec0b3561a52ca31a87652
orozhko <olyarozhko@gmail.com>
2021-12-14 00:17:37 +0200

    hw2_maven

---
+++ src/main/java/hw2_maven/Main.java
@@ -0,0 +1,11 @@
+package hw2_maven;
+
+import hw2_maven2.Person;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person1 = new Person("Ivan", 25);
+
+        person1.whoAmI();
+    }
+}
